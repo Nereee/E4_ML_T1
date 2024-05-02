3d9380db0fcaa3fce5a16cd8b275381eac49dd1a
Iraitz <157578442+Iraitz-Elorrieta@users.noreply.github.com>
2024-05-02 10:54:13 +0200

    Langile Zein Dep filtratuta Ardura falta
    Departamentuak Filtratu egiten dira Baina Ardurak ez... Ardurak filtratu aparte beste orri berri baten

--- MIMI/html/php/langileak.php
+++ MIMI/html/php/langileak.php
@@ -1,16 +1,28 @@
 <?php
+// Obtener el valor de query_string y pasarlo como parámetro al transformar el XML con la hoja de estilos XSLT
+$query_string = $_SERVER['QUERY_STRING'];
 
-   $arauak = new DOMDocument();
-   $arauak ->load("../../datuak/xsl/langileak.xsl");
+// Cargar la hoja de estilos XSLT
+$arauak = new DOMDocument();
+$arauak->load("../../datuak/xsl/langileak.xsl");
 
-   $datuak = new DOMDocument();
-   $datuak->load("../../datuak/xml/datuak.xml");
+// Cargar el XML de datos
+$datuak = new DOMDocument();
+$datuak->load("../../datuak/xml/datuak.xml");
 
-   $proc = new XSLTProcessor();
-   $proc->importStylesheet($arauak);
+// Crear el procesador XSLT
+$proc = new XSLTProcessor();
 
-   echo $proc->transformToXML($datuak);
+// Importar la hoja de estilos XSLT
+$proc->importStylesheet($arauak);
 
+// Establecer el parámetro query_string
+$proc->setParameter('', 'query_string', $query_string);
+
+// Obtener el valor de selectedDepartamentua y pasarlo como parámetro
+$selectedDepartamentua = substr(strstr($query_string, "departamentuakcheckbox="), strlen("departamentuakcheckbox="));
+$proc->setParameter('', 'selectedDepartamentua', $selectedDepartamentua);
+
+// Transformar el XML con la hoja de estilos XSLT y mostrar el resultado
+echo $proc->transformToXML($datuak);
 ?>
-
-
